361886179a5cd0b847db7115ddb20f7ccbbb3393
AnBasement <KristofferAG@gmail.com>
2025-10-02 12:37:48 +0200

    Changing up nav menu and cta buttons.

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,7 +1,15 @@
 const plans = document.getElementById("plans");
 plans.addEventListener("click", () => {
-  plans.classList.add("clicked-plans");
+  plans.classList.add("clicked-button");
   setTimeout(() => {
     window.location.href = "plans.html";
+  }, 300);
+});
+
+const repo = document.getElementById("repo");
+repo.addEventListener("click", () => {
+  repo.classList.add("clicked-button");
+  setTimeout(() => {
+    window.open = "https://github.com/AnBasement/FreeGram";
   }, 300);
 });
